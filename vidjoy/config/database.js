7901cc39b41d5ca21acdf6beac87591643651f27
rajeshwarsingh <rajan23024@gmail.com>
2018-01-03 14:30:28 +0530

    Update database.js

--- vidjoy/config/database.js
+++ vidjoy/config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://rajan:rajan@ds131687.mlab.com:31687/vidjoy-prod'}
+  module.exports = {mongoURI: 'mongodb://admin:admin@ds131687.mlab.com:31687/vidjoy-prod'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost:27017/vidjot-dev'}
 }
